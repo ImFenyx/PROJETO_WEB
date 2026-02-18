e3677d259c3129a4b7cd07ea6d06ee71a7149ff7
Iago Anjos da Silva <103691581+ImFenyx@users.noreply.github.com>
2026-02-17 22:45:01 -0300

    refactor: ♻️ reorganize keyframes, improve main.js script and enhance HTML metadata for better performance

--- site-pessoal/src/assets/js/main.js
+++ site-pessoal/src/assets/js/main.js
@@ -1,10 +1,9 @@
 const track = document.querySelector(".carousel-track");
-const cards = Array.from(track.children);
-cards.forEach((card) => {
-  const clone = card.cloneNode(true);
-  console.log(clone);
-  track.appendChild(clone);
-});
 
-console.log(track);
-console.log(cards);
+if (track) {
+  const cards = Array.from(track.children);
+  cards.forEach((card) => {
+    const clone = card.cloneNode(true);
+    track.appendChild(clone);
+  });
+}
