119f0ed78f9035af6119a76f9bbc4ea799e1c463
Iago Anjos da Silva <103691581+ImFenyx@users.noreply.github.com>
2026-05-28 11:21:13 -0300

    refactor(analise-estatistica): ♻️ used better implementation for concatenating html strings

--- analise-estatistica/src/assets/js/main.js
+++ analise-estatistica/src/assets/js/main.js
@@ -23,72 +23,37 @@ function gerarCampos() {
   }
 
   camposAlunos.innerHTML = "";
+  let htmlResult = "";
 
   for (let i = 0; i < qtde; i++) {
-    let html = "";
-    html +=
-      "<div class='rounded-xl border border-mocha-surface1 bg-mocha-mantle p-4'>";
-    html +=
-      "  <p class='mb-3 text-xs font-bold uppercase tracking-wider text-mocha-overlay1'>Aluno " +
-      (i + 1) +
-      "</p>";
-    html += "  <div class='grid grid-cols-1 gap-3 sm:grid-cols-4'>";
-
-    html += "    <div>";
-    html +=
-      "      <label class='form-label' for='aluno_nome_" + i + "'>Nome</label>";
-    html +=
-      "      <input class='form-input' type='text' id='aluno_nome_" +
-      i +
-      "' name='aluno_nome_" +
-      i +
-      "' placeholder='Ex.: Ana Silva' required />";
-    html += "    </div>";
-
-    html += "    <div>";
-    html +=
-      "      <label class='form-label' for='aluno_nota1_" +
-      i +
-      "'>Prova 1</label>";
-    html +=
-      "      <input class='form-input' type='number' id='aluno_nota1_" +
-      i +
-      "' name='aluno_nota1_" +
-      i +
-      "' min='0' max='10' step='0.1' placeholder='0 a 10' required />";
-    html += "    </div>";
-
-    html += "    <div>";
-    html +=
-      "      <label class='form-label' for='aluno_nota2_" +
-      i +
-      "'>Prova 2</label>";
-    html +=
-      "      <input class='form-input' type='number' id='aluno_nota2_" +
-      i +
-      "' name='aluno_nota2_" +
-      i +
-      "' min='0' max='10' step='0.1' placeholder='0 a 10' required />";
-    html += "    </div>";
-
-    html += "    <div>";
-    html +=
-      "      <label class='form-label' for='aluno_trabalho_" +
-      i +
-      "'>Trabalho</label>";
-    html +=
-      "      <input class='form-input' type='number' id='aluno_trabalho_" +
-      i +
-      "' name='aluno_trabalho_" +
-      i +
-      "' min='0' max='10' step='0.1' placeholder='0 a 10' required />";
-    html += "    </div>";
-
-    html += "  </div>";
-    html += "</div>";
-
-    camposAlunos.innerHTML += html;
+    htmlResult += `
+      <div class='rounded-xl border border-mocha-surface1 bg-mocha-mantle p-4'>
+        <p class='mb-3 text-xs font-bold uppercase tracking-wider text-mocha-overlay1'>
+          Aluno ${i + 1}
+        </p>
+        <div class='grid grid-cols-1 gap-3 sm:grid-cols-4'>
+          <div>
+            <label class='form-label' for='aluno_nome_${i}'>Nome</label>
+            <input class='form-input' type='text' id='aluno_nome_${i}' name='aluno_nome_${i}' placeholder='Ex.: Ana Silva' required />
+          </div>
+          <div>
+            <label class='form-label' for='aluno_nota1_${i}'>Prova 1</label>
+            <input class='form-input' type='number' id='aluno_nota1_${i}' name='aluno_nota1_${i}' min='0' max='10' step='0.1' placeholder='0 a 10' required />
+          </div>
+          <div>
+            <label class='form-label' for='aluno_nota2_${i}'>Prova 2</label>
+            <input class='form-input' type='number' id='aluno_nota2_${i}' name='aluno_nota2_${i}' min='0' max='10' step='0.1' placeholder='0 a 10' required />
+          </div>
+          <div>
+            <label class='form-label' for='aluno_trabalho_${i}'>Trabalho</label>
+            <input class='form-input' type='number' id='aluno_trabalho_${i}' name='aluno_trabalho_${i}' min='0' max='10' step='0.1' placeholder='0 a 10' required />
+          </div>
+        </div>
+      </div>
+    `;
   }
+
+  camposAlunos.innerHTML = htmlResult;
 
   camposAlunos.classList.remove("hidden");
   btnEnviar.classList.remove("hidden");
